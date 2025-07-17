8d575129565cfb4abcdf2bf63244d98c4f2e5ad5
ismailrowaihy <ismailrowaihy@gmail.com>
2025-07-17 06:44:14 +0000

    a function that convers lowe to upper string

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    res = ""
+    for i in str:
+        if (ord(i) in range(ord('a'), ord('z') + 1)):
+            res += (chr(ord(i) - 32))
+        else:
+            res += i
+    print(res)
