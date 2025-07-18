3cf31db2031be727cf444f8c48297c0c7a078bcd
ismailrowaihy <ismailrowaihy@gmail.com>
2025-07-18 16:19:39 +0000

    fixed when more then 1 S

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,7 +4,8 @@ import sys
 if __name__ == "__main__":
 
     if len(sys.argv) > 1:
-        print(f"{len(sys.argv) - 1} argument:")
+        print(f"{len(sys.argv) - 1} argument:") if len(sys.argv) == 2 else\
+         print(f"{len(sys.argv) - 1} arguments:")
 
         for i in range(1, len(sys.argv)):
             print(f"{i}: {sys.argv[i]}")
