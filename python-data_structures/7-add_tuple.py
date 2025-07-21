b8d1114fd12fca7256bdb1bfb5b1fa92d9b463ee
ismailrowaihy <ismailrowaihy@gmail.com>
2025-07-21 07:58:38 +0000

    fixed pycodestyle

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -4,6 +4,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_a += (0,)
     while len(tuple_b) < 2:
         tuple_b += (0,)
-    
-    new_tuple = tuple_a[0] + tuple_b[0],tuple_a[1] + tuple_b[1]
+
+    new_tuple = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
     return (new_tuple)
