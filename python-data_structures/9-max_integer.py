46303aa3b6be21a57ecd9df544e3a3047699fbf4
ismailrowaihy <ismailrowaihy@gmail.com>
2025-07-21 08:22:07 +0000

    fixed if num is negitave

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    is_max = 0
     if not my_list:
         return (None)
+
+    is_max = my_list[0]
 
     for i in my_list:
         if i > is_max:
