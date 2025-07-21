bb0c2172105902b03af119336e620e742ea8dfd1
ismailrowaihy <ismailrowaihy@gmail.com>
2025-07-21 07:26:17 +0000

    not allowed to cast into str fixed

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
-        print(" ".join(str(j) for j in i))
+        for j in range(len(i)):
+            if j != len(i) - 1:
+                print("{} ".format(i[j]),end="")
+            else:
+                print("{}".format(i[j]),end="")
+        print()
+    
+
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print_matrix_integer(matrix)
+print("--")
+print_matrix_integer()
 
