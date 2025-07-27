8dcab3f943b190633e3acb14c7b87192b8198e1e
ismailrowaihy <ismailrowaihy@gmail.com>
2025-07-27 10:10:44 +0000

    fixed default value

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -6,9 +6,9 @@ this module is an emptiy class
 
 class Square:
     """a squre class"""
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         """ initilize function"""
-        if not isinstance(size,int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
