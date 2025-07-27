e6fc9c5ae4e56584e134dce7471b2adcbc3773cb
ismailrowaihy <ismailrowaihy@gmail.com>
2025-07-27 09:47:23 +0000

    added a size to my square class

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+this module is an emptiy class
+"""
+
+
+class Square:
+    """a squre class"""
+    def __init__(self, size):
+        """ initilize function"""
+        self.__size = size
