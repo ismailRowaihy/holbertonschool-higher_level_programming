8f3028a24243e9be1fe60d56ddd6b8d539840469
ismailrowaihy <ismailrowaihy@gmail.com>
2025-07-28 08:40:22 +0000

    testing

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -8,11 +8,7 @@ class Square:
     """a squre class"""
     def __init__(self, size=0):
         """ initilize function"""
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+        self.size = size
 
     @property
     def size(self):
