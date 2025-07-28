ad789fd8acc4b8982f4acd0e7d48345c4acab689
ismailrowaihy <ismailrowaihy@gmail.com>
2025-07-28 07:06:08 +0000

    added a hande for setter if value is less then 0

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -24,6 +24,8 @@ class Square:
         """a setter function for size"""
         if type(value) != int:
             raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = value
 
     def area(self):
