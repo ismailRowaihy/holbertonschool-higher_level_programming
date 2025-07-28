c298f1f9e5f406e6da1b620649b5b1732b595be8
ismailrowaihy <ismailrowaihy@gmail.com>
2025-07-28 13:06:06 +0000

    added an empty Rectangle class

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+this module is an emptiy class
+"""
+
+
+class Rectangle :
+    """a Rectangle  class"""
+    def __init__(self):
+        """ initilize function"""
+        self
