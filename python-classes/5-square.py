379b1976eab4e02a5ff3946c1844d23ed5d18750
ismailrowaihy <ismailrowaihy@gmail.com>
2025-07-28 07:33:21 +0000

    added a method that prints a square with size

---
+++ python-classes/5-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+this module is an emptiy class
+"""
+
+
+class Square:
+    """a squre class"""
+    def __init__(self, size=0):
+        """ initilize function"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        """a getter function for size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """a setter function for size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """a function that returns the area of square"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """a function that prints a # = to size"""
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print()
+        if self.__size == 0:
+            print()
