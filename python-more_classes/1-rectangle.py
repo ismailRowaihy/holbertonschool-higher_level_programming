fb40546a1c3f9ef4ae107f4ad0bb103537cf2fa8
ismailrowaihy <ismailrowaihy@gmail.com>
2025-07-28 13:19:54 +0000

    added 2 fields for a rectangle class

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+this module is an emptiy class
+"""
+
+
+class Rectangle:
+    """a Rectangle  class"""
+    def __init__(self, width=0, height=0):
+        """ initilize function"""
+        self.width = width
+        self.height = height
+        
+    @property
+    def width(self):
+        """a getter for width"""
+        return self.__width
+
+    @property
+    def height(self):
+        """a getter for height"""
+        return self.__height
+
+    @size.setter
+    def width(self, value):
+        """a setter for width"""
+        if  not isinstance(value,int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @size.setter
+    def height(self, value):
+        """a setter for height"""
+        if  not isinstance(value,int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+
