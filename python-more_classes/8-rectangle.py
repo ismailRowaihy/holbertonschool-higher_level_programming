2c936c404ef02556cbbad10143a7cafbfae44e1e
ismailrowaihy <ismailrowaihy@gmail.com>
2025-07-28 17:22:59 +0000

    added a static method that returns the bigger rectangle based on area

---
+++ python-more_classes/8-rectangle.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+"""
+this module is an emptiy class
+"""
+
+
+class Rectangle:
+    """a Rectangle  class"""
+
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        """ initilize function"""
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    def __str__(self):
+        if (self.__height * self.__width) == 0:
+            return ""
+        else:
+            for i in range(self.__height):
+                if i == self.__height - 1:
+                    print(str(self.print_symbol) * self.__width, end="")
+                else:
+                    print(str(self.print_symbol) * self.__width)
+        return ""
+
+    def __del__(self):
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
+
+    def __repr__(self):
+        return f"Rectangle({self.__width}, {self.__height})"
+
+    @property
+    def width(self):
+        """a getter for width"""
+        return self.__width
+
+    @property
+    def height(self):
+        """a getter for height"""
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        """a setter for width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """a setter for height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__height * self.__width
+
+    def perimeter(self):
+        if (self.__height * self.__width) == 0:
+            return 0
+        return 2 * (self.__height + self.__width)
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        return rect_1 if rect_1.area() > rect_2.area() else rect_2
