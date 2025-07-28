6a09edd0686aba56058ac07fc93be9e3f8e38851
ismailrowaihy <ismailrowaihy@gmail.com>
2025-07-28 17:45:09 +0000

    fixed pycodestyle

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -76,7 +76,7 @@ class Rectangle:
             if not isinstance(rect, Rectangle):
                 raise TypeError(f"rect_{i} must be an instance of Rectangle")
         return rect_1 if rect_1.area() >= rect_2.area() else rect_2
-    
+
     @classmethod
     def square(cls, size=0):
-        return Rectangle(size,size)
+        return Rectangle(size, size)
