79942564543ebdcb610ac1e6b2bd168e61a3f536
ismailrowaihy <ismailrowaihy@gmail.com>
2025-07-28 17:25:53 +0000

    bigger OR EQUAL error fixed

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -72,4 +72,4 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
-        return rect_1 if rect_1.area() > rect_2.area() else rect_2
+        return rect_1 if rect_1.area() >= rect_2.area() else rect_2
