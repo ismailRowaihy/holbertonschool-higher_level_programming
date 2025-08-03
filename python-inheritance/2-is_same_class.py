f9c3fcd9909a020db16c299c7a4dec139f2dbf24
ismailrowaihy <ismailrowaihy@gmail.com>
2025-08-03 07:44:39 +0000

    added a func that check if an oject is exactly an instance of a class

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""this Module is a function that
+return is an object is an instance of a class """
+
+
+def is_same_class(obj, a_class):
+    """this Module is a function that
+    return is an object is an instance of a class"""
+    return type(obj) is a_class
