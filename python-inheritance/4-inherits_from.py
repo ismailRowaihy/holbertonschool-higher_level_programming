a189401b68327259d6a725fc56554784bc31cce2
ismailrowaihy <ismailrowaihy@gmail.com>
2025-08-03 08:10:21 +0000

    fixed

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -6,13 +6,4 @@ return is an object is an instance of a class """
 def inherits_from(obj, a_class):
     """this Module is a function that
     return is an object is an instance of a class"""
-    return type(obj) is not a_class and isinstance(obj,a_class)
-
-
-a = True
-if inherits_from(a, int):
-    print("{} inherited from class {}".format(a, int.__name__))
-if inherits_from(a, bool):
-    print("{} inherited from class {}".format(a, bool.__name__))
-if inherits_from(a, object):
-    print("{} inherited from class {}".format(a, object.__name__))
+    return type(obj) is not a_class and isinstance(obj, a_class)
