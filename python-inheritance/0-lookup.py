a3e9b318d99770d78e68b6585c1548867c4cb1df
ismailrowaihy <ismailrowaihy@gmail.com>
2025-08-03 06:29:21 +0000

    added doc

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+"""this Module is a function that returns attributes and methods of an object """
+
+
 def lookup(obj):
+    """this function returns atteibutes and methods of obj"""
     return dir(obj)
