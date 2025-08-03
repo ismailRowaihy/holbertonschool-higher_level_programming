d53889735cf8f86c62daf1f76387eaf6eb6dd0fc
ismailrowaihy <ismailrowaihy@gmail.com>
2025-08-03 08:24:14 +0000

    added a area func that raises an exeption

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""this Module is a empty class """
+
+
+class BaseGeometry:
+    """empty class for now"""
+
+    def __init__(self):
+        self
+
+    def area(self):
+        raise Exception("area() is not implemented")
