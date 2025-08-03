a9cbdff8d308eaec41d9ca081ffb50152577a4d5
ismailrowaihy <ismailrowaihy@gmail.com>
2025-08-03 09:27:49 +0000

    changed to impoerting basegeo

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -2,22 +2,7 @@
 """this Module is a Rectangle class """
 
 
-class BaseGeometry:
-    """empty class for now"""
-
-    def __init__(self):
-        self
-
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-
-        self.name = name
-        if type(value) is not int:
-            raise TypeError(f"{self.name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{self.name} must be greater than 0")
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
