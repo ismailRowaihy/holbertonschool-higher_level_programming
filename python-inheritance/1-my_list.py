b70b965052d4d87d23c33daf199361b39802f817
ismailrowaihy <ismailrowaihy@gmail.com>
2025-08-03 06:43:35 +0000

    added test files

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""this Module is a class that inherits from list """
+"""this Module is a class that inherits class list """
 
 
 class MyList(list):
+    """a class that ingerits class list"""
     def print_sorted(self):
         print(sorted(self))
