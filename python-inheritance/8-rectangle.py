1bf7b53e619fdf8a2414b0f1f11d423b53ca0e90
ismailrowaihy <ismailrowaihy@gmail.com>
2025-08-03 09:21:21 +0000

    adde another class that inherits from baseGeo

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""this Module is a Rectangle class """
+
+
+class BaseGeometry:
+    """empty class for now"""
+
+    def __init__(self):
+        self
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+
+        self.name = name
+        if type(value) is not int:
+            raise TypeError(f"{self.name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{self.name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class"""
+
+    def __init__(self, width, height):
+        super().integer_validator("width", width)
+        super().integer_validator("width", height)
+
+        self.__width = width
+        self.__height = height
