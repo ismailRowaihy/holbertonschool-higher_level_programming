ea73233360dc1c280d856c83d907750a5072ce8c
ismailrowaihy <ismailrowaihy@gmail.com>
2025-08-04 08:15:16 +0000

    test

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -42,8 +42,6 @@ class Rectangle(Shape):
         return self.width * self.height
 
     def perimeter(self):
-        if (self.width * self.height) == 0:
-            return 0
         return 2 * (self.width + self.height)
 
 
