1d2bb42692e0045da341fc098e7e934d2c903c5c
ismailrowaihy <ismailrowaihy@gmail.com>
2025-08-04 08:14:50 +0000

    test

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -24,6 +24,11 @@ class Circle(Shape):
     def __init__(self, radius):
         self.radius = radius
 
+    def area(self):
+        return math.pi * (self.radius ** 2)
+
+    def perimeter(self):
+        return 2 * math.pi * self.radius
 
 
 class Rectangle(Shape):
