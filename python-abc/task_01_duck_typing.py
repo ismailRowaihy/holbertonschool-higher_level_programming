5bab2e8ba1c1f5a829d574929944611d6495f993
ismailrowaihy <ismailrowaihy@gmail.com>
2025-08-04 07:16:08 +0000

    added a shape abstract class and made rectangle and cicle classes that have area and perimeter func

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""this Module is about learning abc class """
+
+
+from abc import ABC, abstractmethod
+import math
+
+
+class Shape(ABC):
+    """an abstract class"""
+
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+
+class Circle(Shape):
+    """a circle class that inherits from shape"""
+
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * (self.radius ** 2)
+
+    def perimeter(self):
+        return 2 * math.pi * self.radius
+
+
+class Rectangle(Shape):
+    """a Rectangle class that inherits from shape"""
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+
+
+def shape_info(Shape):
+    print(f"Area: {Shape.area()}")
+    print(f"Perimeter: {Shape.perimeter()}")
