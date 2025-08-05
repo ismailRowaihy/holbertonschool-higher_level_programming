d51b9653889ae6504576bbd7bcea4ea598e3ca52
ismailrowaihy <ismailrowaihy@gmail.com>
2025-08-05 08:46:51 +0000

    added a class that inherits class list and add more thing to its functions

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""this Module is about learning abc class """
+
+
+from abc import ABC, abstractmethod
+
+
+class VerboseList(list):
+    def __init__(self, aList):
+        super().__init__(aList)
+
+    def append(self, item):
+        super().append(item)
+        print(f"Added [{item}] to the list.")
+
+    def extend(self, x):
+        super().extend(x)
+        print(f"Extended the list with [{x}] items.")
+
+    def remove(self, item):
+        if item not in self:
+            return None
+        super().remove(item)
+        print(f"Removed [{item}] from the list.")
+
+    def pop(self, item=None):
+        if item is not None:
+            item = super().pop(item)
+            print(f"Popped [{item}] from the list.")
+        else:
+            item = super().pop()
+            print(f"Popped [{item}] from the list.")
