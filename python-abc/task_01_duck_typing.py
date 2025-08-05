758eb9452ff8b9b309fb00aed286edd04ca15df9
ismailrowaihy <ismailrowaihy@gmail.com>
2025-08-05 09:47:37 +0000

    testing adding doc to everything

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -12,10 +12,12 @@ class Shape(ABC):
 
     @abstractmethod
     def area(self):
+        """a function"""
         pass
 
     @abstractmethod
     def perimeter(self):
+        """a function"""
         pass
 
 
@@ -23,12 +25,15 @@ class Circle(Shape):
     """a circle class that inherits from shape"""
 
     def __init__(self, radius):
+        """a function"""
         self.radius = radius
 
     def area(self):
+        """a function"""
         return PI * (self.radius ** 2)
 
     def perimeter(self):
+        """a function"""
         return 2 * PI * self.radius
 
 
@@ -36,16 +41,20 @@ class Rectangle(Shape):
     """a Rectangle class that inherits from shape"""
 
     def __init__(self, width, height):
+        """a function"""
         self.width = width
         self.height = height
 
     def area(self):
+        """a function"""
         return self.width * self.height
 
     def perimeter(self):
+        """a function"""
         return 2 * (self.width + self.height)
 
 
 def shape_info(shape):
+    """a function"""
     print("Area: {}".format(shape.area()))
     print("Perimeter: {}".format(shape.perimeter()))
