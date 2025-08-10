094be5d0f036108bfa05993c06f36e691f86733b
ismailrowaihy <ismailrowaihy@gmail.com>
2025-08-10 06:32:53 +0000

    added doc for module and function

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+"""this Module is a function
+that opens and reads then prints a file"""
 
 
 def read_file(filename=""):
+    """a function that opens and reads then prints a file"""
+
     with open(filename, mode="r", encoding="UTF-8") as file:
         txt = file.read()
         print(txt)
