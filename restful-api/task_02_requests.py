b1a6075ec2c126042f207a3f3310ea8fd93f4ec8
ismailrowaihy <ismailrowaihy@gmail.com>
2025-08-18 07:52:05 +0000

    fixed typo

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -8,14 +8,13 @@ data = rr.json()
 
 
 def fetch_and_print_posts():
-    print(f"Status Code : {rr.status_code}")
+    print(f"Status Code: {rr.status_code}")
     for i in data:
         print(i["title"])
 
 
 def fetch_and_save_posts():
     if rr.status_code >= 200 and rr.status_code < 300:
-
         fieldnames = data[0].keys()
         with open("posts.csv", "w") as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
