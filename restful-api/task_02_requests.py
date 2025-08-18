f7d839b553ccc90a5639dc05d052740ae0b7e107
ismailrowaihy <ismailrowaihy@gmail.com>
2025-08-18 07:41:42 +0000

    added 2 func that prints posts title and one that saves posts in csv file

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import requests
+import csv
+import json
+
+rr = requests.get('https://jsonplaceholder.typicode.com/posts')
+data = rr.json()
+
+def fetch_and_print_posts():
+    print (f"Status Code : {rr.status_code}")
+    for i in data:
+        print (i["title"])
+        print ("__")
+
+def fetch_and_save_posts():
+    fieldnames = data[0].keys()
+    with open('posts.csv', 'w') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(data) 
