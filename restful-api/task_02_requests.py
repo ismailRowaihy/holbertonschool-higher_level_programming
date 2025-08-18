ab5e6f4d86bd2c7057006571fbbdb9ea49ef3b10
ismailrowaihy <ismailrowaihy@gmail.com>
2025-08-18 09:41:26 +0000

    added get to each function

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -3,20 +3,27 @@
 import requests
 import csv
 
-rr = requests.get("https://jsonplaceholder.typicode.com/posts")
-data = rr.json()
-Fdata = [{k:v for k,v in x.items() if k != 'userId'} for x in data]
+
 
 def fetch_and_print_posts():
+    rr = requests.get("https://jsonplaceholder.typicode.com/posts")
+    data = rr.json()
     print(f"Status Code: {rr.status_code}")
     for i in data:
         print(i["title"])
 
 
 def fetch_and_save_posts():
+    rr = requests.get("https://jsonplaceholder.typicode.com/posts")
+
+    Fdata = [{k:v for k,v in x.items() if k != 'userId'} for x in data]
     if rr.status_code >= 200 and rr.status_code < 300:
         fieldnames = Fdata[0].keys()
+        
         with open("posts.csv", "w") as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            print(fieldnames)
+            writer = csv.DictWriter(csvfile,fieldnames=fieldnames)
             writer.writeheader()
             writer.writerows(Fdata)
+
+fetch_and_save_posts()
