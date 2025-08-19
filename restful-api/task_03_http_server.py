57f1a7873d4ec0e567c4804ee1719e08de7b9c23
ismailrowaihy <ismailrowaihy@gmail.com>
2025-08-19 08:21:40 +0000

    using http.server to make a simple api calls

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+from http.server import HTTPServer,BaseHTTPRequestHandler
+import json
+
+class HTTPhandler(BaseHTTPRequestHandler):
+    
+    def do_GET(self):
+        if self.path == "/data":
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            data = json.dumps({"name": "John", "age": 30, "city": "New York"})
+            self.wfile.write(bytes(data, "utf8"))
+        elif self.path == "/info":
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            data = json.dumps({"version": "1.0", "description": "A simple API built with http.server"})
+            self.wfile.write(bytes(data, "utf8"))
+        else:
+            self.send_response(404)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            massage = "Endpoint not found"
+            self.wfile.write(bytes(massage, "utf8"))
+
+
+
+server = HTTPServer(("",8000), HTTPhandler)
+server.serve_forever()
+server.server_close()
