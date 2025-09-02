3dab3b8438e0814b23da8d690de7593a13318840
ismailrowaihy <ismailrowaihy@gmail.com>
2025-09-02 09:04:54 +0000

    changed from inner join to left

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,7 @@
 -- script that lists all genres and there counts
 SELECT tg.name as genre,
        COUNT(tsg.show_id) as number_of_shows
-FROM tv_genres tg  INNER JOIN tv_show_genres tsg
-WHERE tg.id = tsg.genre_id
+FROM tv_genres tg  LEFT JOIN tv_show_genres tsg
+ON tg.id = tsg.genre_id
 GROUP BY tg.name
-HAVING COUNT(tsg.show_id) > 0
 ORDER BY number_of_shows DESC;
