7908ecbdc26985341d51e1ecb901d9528fd9cc87
ismailrowaihy <ismailrowaihy@gmail.com>
2025-09-02 06:33:13 +0000

    added scrip that lists all cities of a state

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,9 @@
+-- scrip that lists all cities of cali
+SELECT id,
+       name
+FROM cities
+WHERE cities.state_id = 
+    (SELECT id
+    FROM states
+    WHERE name = "California")
+ORDER BY id ASC;
