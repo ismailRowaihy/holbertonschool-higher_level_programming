27af3667d3b5bad305d07720739f3e031feacc2d
ismailrowaihy <ismailrowaihy@gmail.com>
2025-09-02 08:30:47 +0000

    added script that listss the count of show for each genrea

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- script that lists all genres and there counts
+SELECT tg.name as genre,
+       COUNT(tsg.show_id) as number_of_shows
+FROM tv_genres tg  INNER JOIN tv_show_genres tsg
+WHERE tg.id = tsg.genre_id
+GROUP BY genre
+ORDER BY number_of_shows DESC;
