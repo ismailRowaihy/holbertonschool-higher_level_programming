2c8593dfcfd7b95580352f044f917a43e0212ba1
ismailrowaihy <ismailrowaihy@gmail.com>
2025-09-02 07:56:52 +0000

    added script that lists shows with no genres

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,7 @@
+-- script that lists only shows with no genres
+SELECT ts.title,
+        tsg.genre_id
+FROM tv_shows ts LEFT JOIN tv_show_genres tsg
+ON ts.id = tsg.show_id
+WHERE tsg.genre_id IS NULL
+ORDER BY ts.title ASC , tsg.genre_id ASC;
