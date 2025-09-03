0b46d8d97ad1174341d17e292509e376847e067d
ismailrowaihy <ismailrowaihy@gmail.com>
2025-09-03 07:05:23 +0000

    fixed typo lowercase

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -2,7 +2,7 @@
 SELECT tg.name AS name
 FROM tv_genres tg INNER JOIN tv_show_genres tsg
 ON tg.id = tsg.genre_id 
-INNER join tv_shows ts
+INNER JOIN tv_shows ts
 ON ts.id = tsg.show_id
 WHERE ts.title = "Dexter"
 ORDER BY name ASC;
