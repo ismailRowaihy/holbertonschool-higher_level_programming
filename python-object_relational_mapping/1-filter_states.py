f7b845029cbf9d153d557716b6219cceddb9755a
ismailrowaihy <ismailrowaihy@gmail.com>
2025-09-03 08:55:39 +0000

    fixed pystyle

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -14,7 +14,9 @@ if __name__ == "__main__":
         database=sys.argv[3],
     )
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE states.name LIKE 'N%' ORDER BY states.id ASC")
+    cur.execute("SELECT * FROM states \
+                WHERE states.name LIKE 'N%' \
+                ORDER BY states.id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
