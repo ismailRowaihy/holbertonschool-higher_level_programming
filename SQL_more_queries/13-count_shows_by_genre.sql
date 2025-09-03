2cac12b625977e2e5fa0ef43104055c5c60acd48
ismailrowaihy <ismailrowaihy@gmail.com>
2025-09-03 06:33:35 +0000

    changed the having

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -4,5 +4,5 @@ SELECT tg.name as genre,
 FROM tv_genres tg INNER JOIN tv_show_genres tsg
 ON tg.id = tsg.genre_id
 GROUP BY tg.name
-HAVING COUNT(tsg.show_id) > 0
+HAVING number_of_shows > 0 
 ORDER BY number_of_shows DESC;
