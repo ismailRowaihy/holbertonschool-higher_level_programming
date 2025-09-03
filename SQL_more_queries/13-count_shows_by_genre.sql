4d8f89a743b19fbdb1e31e72e819518007504aa8
ismailrowaihy <ismailrowaihy@gmail.com>
2025-09-03 06:27:13 +0000

    fixed typo

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- script that lists all genres and there counts
-SELECT t.gname as genre,
+SELECT tg.name as genre,
        COUNT(tsg.show_id) as number_of_shows
 FROM tv_genres tg INNER JOIN tv_show_genres tsg
 ON tg.id = tsg.genre_id
