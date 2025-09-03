f32b16eaf23f7cc5269cb404dc7facd46d8926f4
ismailrowaihy <ismailrowaihy@gmail.com>
2025-09-03 08:24:35 +0000

    python script that send a query to a database and prints it

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import MySQLdb
+import sys
+
+conn =MySQLdb.connect(host="localhost",port=3306,user=sys.argv[1],
+                  password=sys.argv[2],database=sys.argv[3])
+cur = conn.cursor()
+cur.execute("SELECT * FROM states ORDER BY id ASC")
+query_rows = cur.fetchall()
+for row in query_rows:
+    print(row)
+cur.close()
+conn.close()
