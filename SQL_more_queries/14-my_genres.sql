ef9bbf0900af3fbb6f97913acf7079094f68a041
ismailrowaihy <ismailrowaihy@gmail.com>
2025-09-03 06:49:15 +0000

    added a script that lists all genres of a show

---
+++ SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,7 @@
+SELECT tg.name AS name
+FROM tv_genres tg INNER JOIN tv_show_genres tsg
+ON tg.id = tsg.genre_id 
+INNER join tv_shows ts
+ON ts.id = tsg.show_id
+WHERE ts.title = "Dexter"
+ORDER BY name ASC;
