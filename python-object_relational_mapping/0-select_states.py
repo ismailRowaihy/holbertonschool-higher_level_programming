08bcfe3ab533971bcc2abe04f64b52f466680121
ismailrowaihy <ismailrowaihy@gmail.com>
2025-09-03 08:32:58 +0000

    fixed pystyle and added doc

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
+"""this Module is a python script
+that sends a query and prints it"""
+
 import MySQLdb
 import sys
 
-conn =MySQLdb.connect(host="localhost",port=3306,user=sys.argv[1],
-                  password=sys.argv[2],database=sys.argv[3])
+conn = MySQLdb.connect(
+    host="localhost",
+    port=3306,
+    user=sys.argv[1],
+    password=sys.argv[2],
+    database=sys.argv[3],
+)
 cur = conn.cursor()
-cur.execute("SELECT * FROM states ORDER BY id ASC")
+cur.execute("SELECT * FROM states ORDER BY states.id ASC")
 query_rows = cur.fetchall()
 for row in query_rows:
     print(row)
