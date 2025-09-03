f81834ff4b57a92fc851016b2782c0e08f6b9992
ismailrowaihy <ismailrowaihy@gmail.com>
2025-09-03 07:10:39 +0000

    added script that lists all shows of a genra

---
+++ SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,8 @@
+-- script that lists all shows of a genrea
+SELECT ts.title AS title
+FROM tv_shows ts INNER JOIN tv_show_genres tsg
+ON ts.id = tsg.show_id
+INNER JOIN tv_genres tg
+ON tg.id = tsg.genre_id
+WHERE tg.name = "Comedy"
+ORDER BY title ASC;
