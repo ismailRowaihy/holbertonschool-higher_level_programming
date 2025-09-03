f3d334f24d3052c9f9e71967bb70e794bb8cc006
ismailrowaihy <ismailrowaihy@gmail.com>
2025-09-03 08:42:27 +0000

    made the file doesnt execute when imported

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,17 +5,18 @@ that sends a query and prints it"""
 import MySQLdb
 import sys
 
-conn = MySQLdb.connect(
-    host="localhost",
-    port=3306,
-    user=sys.argv[1],
-    password=sys.argv[2],
-    database=sys.argv[3],
-)
-cur = conn.cursor()
-cur.execute("SELECT * FROM states ORDER BY states.id ASC")
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        password=sys.argv[2],
+        database=sys.argv[3],
+    )
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
