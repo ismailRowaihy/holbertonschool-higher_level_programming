3f16973bdaaee252f7b42e5627de1b7bb872bf54
ismailrowaihy <ismailrowaihy@gmail.com>
2025-09-06 06:01:18 +0000

    changed check for exactly =

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     cur = conn.cursor()
     cur.execute(
         "SELECT * FROM states \
-         WHERE states.name = '{}' \
+         WHERE BINARY states.name = '{}' \
          ORDER BY states.id ASC".format(sys.argv[4])
     )
     query_rows = cur.fetchall()
