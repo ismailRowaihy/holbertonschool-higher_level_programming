34f60f2ea9f46a47898430f23a1580f78ccb94cf
ismailRowaihy <ismailRowaihy@gmail.com>
2025-09-20 08:01:43 +0000

    script that prints the second biggest num

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+const { argv } = require('node:process');
+if (argv.length < 3) {
+  console.log(0);
+} else {
+  let biggestNum = parseInt(argv[2], 10);
+  let scndBiggestNum = 0;
+
+  argv.slice(3).forEach((val) => {
+    const num = parseInt(val, 10);
+    if (num > biggestNum) {
+      scndBiggestNum = biggestNum;
+      biggestNum = num;
+    } else if (num > scndBiggestNum) {
+      scndBiggestNum = num;
+    }
+  });
+  console.log(scndBiggestNum);
+}
