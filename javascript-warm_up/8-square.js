0c45dcde779eee94bb7e37f2d833cf9439f3590a
ismailRowaihy <ismailRowaihy@gmail.com>
2025-09-20 07:29:07 +0000

    added script that prints a squre

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const { argv } = require('node:process');
+const isNum = parseInt(argv[2], 10);
+
+if (isNaN(isNum)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < isNum; i++) {
+    console.log('#'.repeat(isNum));
+  }
+}
