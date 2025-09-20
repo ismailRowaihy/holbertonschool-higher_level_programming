16242210b810f2f664db7661ba99999d42d5ee20
ismailRowaihy <ismailRowaihy@gmail.com>
2025-09-20 07:46:52 +0000

    script that does factorial

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+const { argv } = require('node:process');
+const isNum = parseInt(argv[2], 10);
+
+function fact (num) {
+  if (num <= 0 || isNaN(num)) {
+    return 1;
+  }
+  return num * fact(num - 1);
+}
+
+console.log(fact(isNum));
