390b35aca903c964b147bd09cbb7f25b8fb44182
ismailRowaihy <ismailRowaihy@gmail.com>
2025-09-21 09:07:22 +0000

    fixed semistandered

--- javascript-dom_manipulation/4-script.js
+++ javascript-dom_manipulation/4-script.js
@@ -2,7 +2,7 @@ const myListElement = document.querySelector('.my_list');
 const addItem = document.getElementById('add_item');
 
 addItem.addEventListener('click', function () {
-    const newElem = document.createElement('li');
-    newElem.textContent = 'Item';
-    myListElement.appendChild(newElem);
+  const newElem = document.createElement('li');
+  newElem.textContent = 'Item';
+  myListElement.appendChild(newElem);
 });
